7e9af4836ddd49285f713f5a90ad3d74ef80d497
“egbe <egbea123@hotmail.com>
2020-09-23 14:36:32 -0400

    Initial commit

---
+++ src/org/launchcode/Main.java
@@ -0,0 +1,10 @@
+package org.launchcode;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World!");
+        System.out.println("I'm here to explore");
+    }
+}
